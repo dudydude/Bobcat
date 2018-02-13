0689f19ea091433ed2aff66fc4ee0b985d136300
Arthur de Carné <arthurdc09@gmail.com>
2018-02-13 23:40:16 +0100

    map

--- routes/map.js
+++ routes/map.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
-const Venue = require("../models/venue");
 
 router.get("/", function(req, res, next) {
   res.render("events/map");
