d9b8fd0b4e87ce3f71db0eb1093347c3ed3a2091
Josh Vardey <jocelynvardey@gmail.com>
2018-02-14 11:30:09 +0100

    event page

--- routes/map.js
+++ routes/map.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
-=======
-const ensureLogin = require("connect-ensure-login");
-const Venue = require("../models/venue");
->>>>>>> 3ddffc3462b6c2be75b533676ba04dc5ffe86d0e
 
 router.get("/", ensureLoggedIn(), (req, res, next) => {
   res.render("events/map");
