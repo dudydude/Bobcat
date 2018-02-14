0c455f9a09578183f67dfb186ed451a3a25bd4f3
Arthur de Carné <arthurdc09@gmail.com>
2018-02-14 20:11:12 +0100

    working on maps

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -3,50 +3,51 @@ var autocomplete, map, marker;
 // var markerCenter = { lat: myLatLng[0].loc.lat, lng: myLatLng[0].loc.lng };
 // console.log(markerCenter);
 
-// console.log("it works");
+console.log(myLatLng);
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 48.860342, lng: 2.341932 },
     zoom: 14
   });
 
-  //  console.log(jsons1);
-
-  //  console.log("DEBUG VENUE_LIST" + venue_list);
   for (i = 0; i <= myLatLng.length; i++) {
     var marker = new google.maps.Marker({
       position: { lat: myLatLng[i].loc.lat, lng: myLatLng[i].loc.lng },
       map: map
     });
-
-    var contentString =
-      '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      '<div id="bodyContent">' +
-      "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      "sandstone rock formation in the southern part of the " +
-      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      "Aboriginal people of the area. It has many springs, waterholes, " +
-      "rock caves and ancient paintings. Uluru is listed as a World " +
-      "Heritage Site.</p>" +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-      "(last visited June 22, 2009).</p>" +
-      "</div>" +
-      "</div>";
-
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
   }
 
-  var infoWindow = new google.maps.InfoWindow({ map: map });
+  // var contentString =
+  //   '<div id="content">' +
+  //   '<div id="siteNotice">' +
+  //   "</div>" +
+  //   '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+  //   '<div id="bodyContent">' +
+  //   "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+  //   "sandstone rock formation in the southern part of the " +
+  //   "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+  //   "south west of the nearest large town, Alice Springs; 450&#160;km " +
+  //   "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+  //   "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+  //   "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+  //   "Aboriginal people of the area. It has many springs, waterholes, " +
+  //   "rock caves and ancient paintings. Uluru is listed as a World " +
+  //   "Heritage Site.</p>" +
+  //   '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+  //   "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+  //   "(last visited June 22, 2009).</p>" +
+  //   "</div>" +
+  //   "</div>";
+
+  // Generate a marker for each iteration
+
+  // var infowindow = new google.maps.InfoWindow({});
+
+  // marker.addListener("click", function() {
+  //   infowindow.setContent(this.contentString);
+  //   infowindow.open(map, this);
+  //   map.setCenter(this.getPosition());
+  // });
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,40 @@ const express = require("express");
 const router = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
 const Venue = require("../models/venue");
+const Event = require("../models/event");
 
 router.get("/", (req, res, next) => {
+  // Venue.find({}, function(err, venues) {
+  //   res.render("events/map", {
+  //     title: "ça marche",
+  //     venue: venues
+  //   });
+  // });
+
   Venue.find({}, function(err, venues) {
-    if (req.user)
-      res.render("events/map", { title: "ça marche", venue: venues });
-    else res.render("index");
+    console.log(venues);
+    if (err) return next(err);
+    const test = venues.map(element => {
+      return element.name;
+    });
+    console.log(test);
+
+    Event.find({ venue: test }, function(err, events) {
+      if (err) return next(err);
+      // console.log("venue loc" + test);
+      res.render("events/map", {
+        title: "ça marche",
+        venue: venues,
+        event: events
+      });
+    });
+
+    //   var eventsList = events;
+    //   for (i = 0; i < events.length; i++) {
+    //     var eventVenue = eventsList[i].venue;
+    //     console.log("LOG EVENTS ==>" + eventVenue);
+    //   }
+    // } else res.render("index");
   });
 });
 
