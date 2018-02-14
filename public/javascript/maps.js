cbac4991b3a25a40abe14904a9ababf50f36297d
Arthur de Carné <arthurdc09@gmail.com>
2018-02-14 16:57:53 +0100

    marker ok

--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -1,5 +1,8 @@
 var autocomplete, map, marker;
 
+// var markerCenter = { lat: myLatLng[0].loc.lat, lng: myLatLng[0].loc.lng };
+// console.log(markerCenter);
+
 // console.log("it works");
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
@@ -7,14 +10,41 @@ function initMap() {
     zoom: 14
   });
 
-  var json1 = marker;
-  const myLatLng = [];
+  //  console.log(jsons1);
+
   //  console.log("DEBUG VENUE_LIST" + venue_list);
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map
-    //title: { venue_name: venue.name }
-  });
+  for (i = 0; i <= myLatLng.length; i++) {
+    var marker = new google.maps.Marker({
+      position: { lat: myLatLng[i].loc.lat, lng: myLatLng[i].loc.lng },
+      map: map
+    });
+
+    var contentString =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+      '<div id="bodyContent">' +
+      "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+      "sandstone rock formation in the southern part of the " +
+      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+      "south west of the nearest large town, Alice Springs; 450&#160;km " +
+      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+      "Aboriginal people of the area. It has many springs, waterholes, " +
+      "rock caves and ancient paintings. Uluru is listed as a World " +
+      "Heritage Site.</p>" +
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+      "(last visited June 22, 2009).</p>" +
+      "</div>" +
+      "</div>";
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+  }
 
   var infoWindow = new google.maps.InfoWindow({ map: map });
 
@@ -61,10 +91,6 @@ function initialize() {
     document.getElementById("adress").value = venue.formatted_address;
     document.getElementById("venue_lat").value = venue.geometry.location.lat();
     document.getElementById("venue_long").value = venue.geometry.location.lng();
-    //alert("This function is working!");
-    alert(venue.formatted_address);
-    alert(venue.name);
-    // alert(place.address_components[0].long_name);
   });
 }
 //google.maps.event.addDomListener(window, "load", initialize);
