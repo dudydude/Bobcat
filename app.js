56d577ebf948b4b29de450f90a756aacfdb6c2df
Josh Vardey <jocelynvardey@gmail.com>
2018-02-16 14:52:35 +0100

    stylez

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect("mongodb://localhost/bobcat");
 
 const app = express();
 
